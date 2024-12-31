d7266a9c1c57b01fe660cefceeebc8a04156e49e
notAnOldCat <imnotoldcat@gmail.com>
2024-12-31 22:45:05 +0100

    Implemeted enemies spawn + vertical movement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 // * main.rs : fichier source de l'application
 
 /*
-    Palette utilisée pour le jeu: https://lospec.com/palette-list/citrink
+    Palette utilisée pour le jeu:
+        - https://lospec.com/palette-list/citrink (Joueur & Background)
+        - https://lospec.com/palette-list/spooky6 (Ennemi)
 */
 
 // Rend Clippy plus méticuleux
@@ -14,9 +16,9 @@
 
 // Utilise tout ce qui se trouve dans le module 'prelude' du crate 'macroquad'
 use macroquad::prelude::*;
-// use rand::{rand, RandGenerator};
+// use rand::ChooseRandom;
 
-// * Structures
+// * Structures & implémentations
 struct Shape {
     size: f32,
     speed: f32,
@@ -46,14 +48,16 @@ async fn main() {
 
     // Dès lors qu'un littéral est suffisamment long, il est recommandé d'utiliser le symbole '_' comme séparateur.
     let color_background: Color = Color::from_hex(0x254_d70);
-    // let mut squares : Vec<Shape> = vec![];
+    // let enemy_colors = vec![Color::from_hex(0x35014b), Color::from_hex(0x7c0a72), Color::from_hex(0xbe1d47)];
+
+    let mut squares: Vec<Shape> = vec![];
 
     // Représente le joueur
     let mut circle: Shape = Shape::new(
         CIRCLE_RADIUS * 2.0,
         MOVEMENT_SPEED,
-        screen_width() / 2.0 - CIRCLE_RADIUS / 2.0,
-        screen_height() / 2.0 - CIRCLE_RADIUS / 2.0,
+        screen_width() / 2.0,
+        screen_height() / 2.0,
         Color::from_hex(0xfcf_660),
     );
 
@@ -68,6 +72,24 @@ async fn main() {
 
         // Utiliser le temps écoulé depuis la dernière frame permet de convertir le mouvement par frame en mouvement par seconde.
         let delta_time = get_frame_time();
+
+        // Chaque frame, un carré a un certain pourcentage de chances d'être généré
+        if rand::gen_range(0, 99) >= 95 {
+            let size: f32 = rand::gen_range(16.0, 64.0);
+            // let color = enemy_colors.choose();
+            squares.push(Shape::new(
+                size,
+                rand::gen_range(100.0, 300.0),
+                rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                -size + size / 2.0,
+                Color::from_hex(0xbe1_d47),
+            ));
+        }
+
+        // Ajuste la vélocité des ennemis toutes les secondes
+        for square in &mut squares {
+            square.y += square.speed * delta_time;
+        }
 
         // Reçoit les inputs du joueur et ajuste sa vélocité en fonction.
         if is_key_down(KeyCode::Right) {
@@ -95,7 +117,25 @@ async fn main() {
             screen_height() - circle.size / 2.0,
         );
 
-        // Actualise la position du joueur.
+        // Supprime un ennemi dès lors qu'il franchit le bord inférieur de la fenêtre de jeu.
+        squares.retain(|square| square.y < screen_height() + square.size);
+
+        // Effectue le rendu des ennemis à l'écran.
+        for square in &squares {
+            draw_rectangle_ex(
+                square.x,
+                square.y,
+                square.size,
+                square.size,
+                DrawRectangleParams {
+                    offset: Vec2::new(0.5, 0.5),
+                    rotation: 0.0,
+                    color: square.color,
+                },
+            );
+        }
+
+        // Effectue le rendu du joueur à l'écran.
         draw_circle(circle.x, circle.y, circle.size / 2.0, circle.color);
 
         next_frame().await;
